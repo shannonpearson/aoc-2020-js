44b0b1693c99aec5547d75138bda3284dc4c1caf
shannonpearson <shannon.pearson@doctolib.com>
2020-12-01 10:44:20 +0100

    day1 part2 solution

---
+++ day1/part2.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+const data = fs.readFileSync('./part1_input.txt').toString().split('\n').map(s => parseInt(s))
+
+for (let i = 0; i < data.length - 2; i += 1) {
+  for (let j = i + 1; j < data.length - 1; j += 1) {
+    for (let k = j + 1; k < data.length; k += 1) {
+      if (data[i] + data[j] + data[k] === 2020) {
+        return console.log(data[i]*data[j]*data[k])
+      }
+    }
+  }
+}
