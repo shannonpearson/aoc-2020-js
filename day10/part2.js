d2d7699b2c79b8e9ee129f33ea4e3dbd2fb9923e
shannonpearson <shannon.pearson@doctolib.com>
2020-12-14 22:47:16 +0100

    day10 part2 v2

--- day10/part2.js
+++ day10/part2.js
@@ -1,25 +1,11 @@
 const { count } = require('console')
 const fs = require('fs')
 const data = fs.readFileSync('./input.txt', 'utf8').split(/\n/).map(s => parseInt(s, 10)).sort((a, b) => a-b)
-data.unshift(0)
 
-const getPrevious = i => [i - 1, i - 2, i - 3].filter(x => x >= 0 && data[i] - data[x] < 4)
+const results = { 0: 1 }
 
-const dict = {}
-data.forEach((n, i) => {
-  const p = getPrevious(i)
-  dict[i] = { preceding: p }
+data.forEach((n) => {
+  results[n] = (results[n - 1] || 0) + (results[n - 2] || 0) + (results[n - 3] || 0)
 })
 
-Object.keys(dict).sort((a, b) => a-b).forEach(k => {
-  const value = dict[k]
-  value.count = 1;
-  if (value.preceding.length) {
-    value.count = 0;
-    value.preceding.forEach(i => {
-      value.count += dict[i].count
-    })
-  }
-})
-
-console.log(dict)
+console.log(results[data[data.length - 1]])
